1141ec377bc6818b3fbdcdc4b8817b09c5768d59
Adrian Quintana <adrian.perez@ucl.ac.uk>
2015-12-15 14:29:05 +0000

    removing initial values

--- src/main/java/org/geppetto/model/neuroml/utils/modeltree/PopulateSummaryNodesModelTreeUtils.java
+++ src/main/java/org/geppetto/model/neuroml/utils/modeltree/PopulateSummaryNodesModelTreeUtils.java
@@ -196,7 +196,7 @@ public class PopulateSummaryNodesModelTreeUtils
 		descriptionVariable.setId(Resources.MODEL_DESCRIPTION.getId());
 		descriptionVariable.setName(Resources.MODEL_DESCRIPTION.get());
 		descriptionVariable.getTypes().add(access.getType(TypesPackage.Literals.HTML_TYPE));
-		descriptionVariable.getInitialValues().put(access.getType(TypesPackage.Literals.HTML_TYPE), html);
+		//descriptionVariable.getInitialValues().put(access.getType(TypesPackage.Literals.HTML_TYPE), html);
 
 		return descriptionVariable;
 	}
