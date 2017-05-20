7d4a1ccd128cd713eee85fed71d8c114e13e0853
Rob Court <rcourt@ed.ac.uk>
2017-05-20 11:45:16 +0100

    Revert "swap out java8 string join for apache commons join"
    This reverts commit 4a4248578fdebf01a6db35b44c477b3a6494904a.

--- src/main/java/org/geppetto/model/neuroml/features/DefaultViewCustomiserFeature.java
+++ src/main/java/org/geppetto/model/neuroml/features/DefaultViewCustomiserFeature.java
@@ -19,7 +19,6 @@ import org.geppetto.core.model.GeppettoModelAccess;
 import org.geppetto.core.services.GeppettoFeature;
 import org.geppetto.core.features.IDefaultViewCustomiserFeature;
 import org.lemsml.jlems.core.type.Component;
-import org.apache.commons.lang3.StringUtils;
 
 public class DefaultViewCustomiserFeature implements IDefaultViewCustomiserFeature
 {
@@ -73,7 +72,7 @@ public class DefaultViewCustomiserFeature implements IDefaultViewCustomiserFeatu
                             domainModel = domainModel.getParent();
                         }
                         Collections.reverse(path_segs);
-                        String path = StringUtils.join(path_segs, ".");
+                        String path = String.join(".", path_segs);
                         colorMap.put(path, hexColor);
                     }
             }
