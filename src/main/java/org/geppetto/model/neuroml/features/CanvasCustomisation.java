3fdfee4321e33482f1ae416c7e8c157fcfbcf0f5
Matt Earnshaw <matt@earnshaw.org.uk>
2017-06-07 13:16:51 +0100

    initial radius fetching

--- src/main/java/org/geppetto/model/neuroml/features/CanvasCustomisation.java
+++ src/main/java/org/geppetto/model/neuroml/features/CanvasCustomisation.java
@@ -9,14 +9,21 @@ class CanvasCustomisation {
     @Expose public String widgetType = "CANVAS";
     @Expose private Map<String, Map<String, String>> componentSpecific = new HashMap<String, Map<String, String>>();
     private Map<String, String> colorMap = new HashMap<String, String>();
+    private Map<String, String> radiusMap = new HashMap<String, String>();
 
     public CanvasCustomisation() {
         componentSpecific.put("colorMap", colorMap);
+        componentSpecific.put("radiusMap", radiusMap);
     }
     
     public void addColor(String path, String color)
     {
         colorMap.put(path, color);
+    }
+
+    public void addRadius(String path, String radius)
+    {
+        radiusMap.put(path, radius);
     }
 
 }
